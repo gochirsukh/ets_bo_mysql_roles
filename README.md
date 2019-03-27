80b8d1eb173c3ed24eaecd90bd304513a9f10f42
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2019-03-27 18:00:47 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ets_bo_mysql_roles
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: ets_bo_mysql_roles
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
